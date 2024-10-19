401c691c5a4d8d3505e3387fa516923e011ba77e
CeciliaVi7 <ceci_bebe13@hotmail.com>
2024-10-19 08:33:04 -0300

    Cerrar Sesion+Detalle.hmtl

--- blog/apps/posts/views.py
+++ blog/apps/posts/views.py
@@ -10,6 +10,12 @@ def posts(request):
     print(noticias)
     return render(request, "posts/posts.html", ctx)
 
+def post_id(request, id):
+    ctx={}
+    noticia= Posts.objects.get(id=id)
+    ctx["noticia"]=noticia
+    return render(request, "posts/detalle.html", ctx)
+
 def about_us(request):
     return render(request, "posts/about_us.html")
 
@@ -19,9 +25,9 @@ def about_us(request):
 #vista basada en clase
 from.form import RegistroForm
 from django.views.generic import CreateView
-#from django.urls import reverse_lazy
+from django.urls import reverse_lazy
 
 class Registro(CreateView):
     form_class=RegistroForm
-    #success_url=reverse_lazy("noticia")
+    success_url=reverse_lazy("noticias")
     template_name= "usuarios/registro.html"
--- blog/blog/settings/base.py
+++ blog/blog/settings/base.py
@@ -11,9 +11,15 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+from django.urls import reverse_lazy
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+LOGIN_REDIRECT_URL=reverse_lazy("noticias")
+LOGOUT_REDIRECT_URL=reverse_lazy("noticias")
+
+LOGIN_URL=reverse_lazy("login")
 
 
 # Quick-start development settings - unsuitable for production
